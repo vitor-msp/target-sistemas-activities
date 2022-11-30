438c4f4b0316575ad1b19c5adcc83a292716fda4
Vitor Mateus <vitor7jan@gmail.com>
2022-11-29 21:36:26 -0300

    feat(question 1): implementted solution of the question 1

---
+++ question-1.js
@@ -0,0 +1,14 @@
+const calculateSum = () => {
+  let index = 13,
+    sum = 0,
+    k = 0;
+
+  while (k < index) {
+    k += 1;
+    sum += k;
+  }
+
+  console.log(`The result is: ${sum}`);
+};
+
+calculateSum();
