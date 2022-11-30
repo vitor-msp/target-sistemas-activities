ea99ccb016ab7f75f0fc1b3bc9a4790b24085be1
Vitor Mateus <vitor7jan@gmail.com>
2022-11-30 00:07:51 -0300

    feat(question 3): implementted solution of the question 3

---
+++ question-3/question-3.ts
@@ -0,0 +1,64 @@
+import { INVOICING_ENTRIES } from "./invoicing";
+
+type dailyBilling = {
+  day: number;
+  value: number;
+};
+
+class Invoicing {
+  private billingAverage: number = 0;
+
+  constructor(private readonly entries: dailyBilling[]) {}
+
+  private sortBillings(): void {
+    this.entries.sort((a, b) => a.value - b.value);
+  }
+
+  public getHighestBilling(): dailyBilling {
+    this.sortBillings();
+    return this.entries[this.entries.length - 1];
+  }
+
+  public getLowestBilling(): dailyBilling {
+    this.sortBillings();
+    return this.entries[0];
+  }
+
+  private filterNonNullBillings(): dailyBilling[] {
+    return this.entries.filter((entry) => entry.value > 0);
+  }
+
+  public calculateBillingAverage(): void {
+    const filteredBillings = this.filterNonNullBillings();
+    const count = filteredBillings.length;
+    const total: number = filteredBillings.reduce<number>(
+      (accumulator: number, current: dailyBilling) => {
+        accumulator += current.value;
+        return accumulator;
+      },
+      0
+    );
+    this.billingAverage = total / count;
+  }
+
+  public getCountDaysWithAboveAverageBilling(): number {
+    const daysWithAboveAverageBilling = this.entries.filter(
+      (entry) => entry.value > this.billingAverage
+    );
+    return daysWithAboveAverageBilling.length;
+  }
+}
+
+const invoicing = new Invoicing(INVOICING_ENTRIES);
+console.log(
+  `The lowest billing: ${JSON.stringify(invoicing.getLowestBilling())}`
+);
+console.log(
+  `The highest billing: ${JSON.stringify(invoicing.getHighestBilling())}`
+);
+invoicing.calculateBillingAverage();
+console.log(
+  `The count of the days with above average billing: ${JSON.stringify(
+    invoicing.getCountDaysWithAboveAverageBilling()
+  )}`
+);
